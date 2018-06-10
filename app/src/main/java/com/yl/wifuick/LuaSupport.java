42a6eae4fec4dff2c93eb1ae8bba9c4cdade4f14
yldylg <yangleidylg@hotmail.com>
2018-06-11 00:12:54 +0800

    rename project id

--- app/src/main/java/com/yl/wifuick/LuaSupport.java
+++ app/src/main/java/com/yl/wifuick/LuaSupport.java
@@ -1,4 +1,4 @@
-package com.yl.wifisniffer;
+package com.yl.wifuick;
 
 
 import java.io.*;
--- app/src/main/java/com/yl/wifuick/WebActivity.java
+++ app/src/main/java/com/yl/wifuick/WebActivity.java
@@ -1,4 +1,4 @@
-package com.yl.wifisniffer;
+package com.yl.wifuick;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
